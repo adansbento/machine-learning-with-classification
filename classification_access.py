ed075dec120f8451124bfd5cabd630937cd6243c
Adans Bento <adansbento@gmail.com>
2018-02-06 22:09:01 -0200

    Importando, classificando e validando um modelo

---
+++ classification_access.py
@@ -0,0 +1,6 @@
+from datas import load_access
+
+X,Y = load_access()
+
+print(X)
+print(Y)
---
+++ datas.py
@@ -0,0 +1,17 @@
+import csv
+
+
+def load_access():
+
+    datas = []
+    making = []
+
+    file = open('page_access.csv', 'rb')
+    reader = csv.reader(file)
+    reader.next()
+
+    for home,how_work,contact, bought in reader:
+        datas.append([ int(home),int(how_work), int(contact) ])
+        making.append( int(bought))
+
+    return datas,making
