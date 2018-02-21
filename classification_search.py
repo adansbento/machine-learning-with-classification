6e446386378e13b830d8c542156aba83828b8591
Adans Bento <adansbento@gmail.com>
2018-02-20 22:20:13 -0300

    Treinando e testando com buscas categoricas

--- classification_search.py
+++ classification_search.py
@@ -8,9 +8,34 @@ Y_df = df['bought']
 Xdummies_df = pd.get_dummies(X_df)
 Ydummies_df = Y_df
 
-X= Xdummies_df.values
-Y= Ydummies_df.values
+X = Xdummies_df.values
+Y = Ydummies_df.values
 
-print(X)
+# perc/100
+fit_size_percent = 0.01
 
-print(Y)
+fit_size = fit_size_percent * len(Y)
+fit_data = X[:int(fit_size)]
+fit_making = Y[:int(fit_size)]
+
+test_size = len(Y) - fit_size
+test_data = X[- int(test_size):]
+test_making = Y[- int(test_size):]
+
+
+from sklearn.naive_bayes import MultinomialNB
+
+model1 = MultinomialNB()
+model1.fit(fit_data,fit_making)
+
+result = model1.predict(test_data)
+diff = test_making - result
+
+#Acertos hits
+hits = [d for d in diff if d==0]
+elements_amount = len(test_data)
+hits_amount = len(hits)
+hits_percent = 100.0*hits_amount/elements_amount
+
+print 'Acertos/Total:',hits_amount,'/', elements_amount
+print 'Acertos em %:',hits_percent
